ccc1c6b396d19dc36c4790b5c4643389eba23a32
Jay Lee <stingpublic@gmail.com>
2025-07-29 17:19:40 +0900

    레이아웃 PC버전 추가 및 조정

--- stt.py
+++ stt.py
@@ -61,5 +61,6 @@ def ask():
         return jsonify({"full":"질문이 비어 있습니다.","summary":""})
     return jsonify(query_assistant(q))
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
